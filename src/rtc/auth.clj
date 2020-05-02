8ba1c41a95b8fbae6eff01a264ea5541b1a59fa5
Coby Tamayo <acobster@gmail.com>
2020-05-01 21:58:29 -0700

    add 2FA login-flow support code

--- src/rtc/auth.clj
+++ src/rtc/auth.clj
@@ -24,15 +24,50 @@
   (authenticate-user "rtc-admin@example.com" "garbage"))
 
 
-(defn unauthorized-handler [req _metadata]
+(defn login-step [{:keys [form-params] :as req}]
   (cond
-    ;; authenticated, but not authorized
+    (and (authenticated? req) (two-factor/verified? req))
+    :logged-in
+
+    (and (authenticated? req)
+         (get form-params "token"))
+    :verifying
+
     (authenticated? req)
-    {:status 403
-     :headers {"Content-Type" "text/plain"}
-     :body "Forbidden"}
+    :two-factor
+
+    (and (get form-params "email")
+         (get form-params "password"))
+    :authenticating
+
     :else
-    (redirect (format "/login?next=%s" (:uri req)))))
+    :unauthenticated))
+
+(defn login-uri [{:keys [uri query-params] :as req}]
+  (let [logged-in? (= :logged-in (login-step req))]
+    (if logged-in?
+      (get query-params "next" "/admin/volunteer")
+      (let [dest (get query-params "next" uri)]
+        (format "/login?next=%s" dest)))))
+
+(defn authenticate-request [{:keys [form-params] :as req}]
+  (let [user (authenticate-user (get form-params "email")
+                                (get form-params "password"))]
+    (println user)
+    (if user
+      (-> (redirect (login-uri req))
+          (assoc :session {:identity user}))
+      (layout/login-page req))))
+
+(defn verify-two-factor-request [{:keys [form-params] :as req}]
+  (if (two-factor/verify-2fa-token (get form-params "token"))
+    (-> (redirect (login-uri req))
+        (assoc-in [:session :verified-2fa-token?] true))
+    (layout/two-factor-page req)))
+
+(defn unauthorized-handler [req _metadata]
+  (redirect (login-uri req)))
+
 
 (defn logout-handler [req]
   (-> (redirect "/login")
