d95bdba0948e0f985e6585b537e86dbacd9ea984
Coby Tamayo <acobster@gmail.com>
2020-05-31 11:09:51 -0700

    availability queries by state/from/to

--- src/rtc/appointments/avail.clj
+++ src/rtc/appointments/avail.clj
@@ -4,6 +4,7 @@
 (ns rtc.appointments.avail
   (:require
    [clj-time.coerce :as c]
+   [honeysql.core :as sql]
    [rtc.db :as d]))
 
 
@@ -14,18 +15,54 @@
 (defn window-resolver [{:keys [from to state]}]
   [{:start_time ""
     :end_time ""
-    :provider {}}])
+    :provider (d/get-provider {:id 5})}])
+
+(defn params->query
+  "Takes a map of params and returns a HoneySQL query map"
+  [{:keys [from to state]}]
+  {:select [:*]
+   :from [[:availabilities :a]]
+   :join
+   (when state [[:providers :p] [:= :p.id :a.provider_id]])
+   :where
+   (filter some? [:and
+                  (when state [:= :state state])
+                  (when (and from to) [:between :start_time (c/to-sql-time from) (c/to-sql-time to)])])})
 
 (defn get-availabilities [params]
-  (let [params (into {} (filter (comp some? val) params))]
-    ))
-
-(into {} (filter (comp some? val) {:a nil :b 'b :c nil :d 'd}))
-(boolean (seq {}))
+  (-> params params->query sql/format d/query))
 
 
 (comment
   (d/bind!)
+
+  (sql/format {:select [:*]
+               :from [:appointments]
+               :where [:and [:between :start_time "2020-01-01" "2020-01-07"] [:= :provider_id 123]]})
+  ;; => ["SELECT * FROM appointments WHERE (start_time BETWEEN ? AND ? AND provider_id = ?)"
+  ;;     "2020-01-01"
+  ;;     "2020-01-07"
+  ;;     123]
+
+  ;; NOTE:
+  ;; `AND` collapses with only a single clause:
+  (sql/format {:select [:*]
+               :from [:appointments]
+               :where [:and [:= 1 1]]})
+  ;; => ["SELECT * FROM appointments WHERE (? = ?)" 1 1]
+
+
+  (d/query (sql/format {:select [:*]
+                        :from [:availabilities]
+                        :where [:and
+                                [:between
+                                 :start_time
+                                 (c/to-sql-time #inst "2020-01-01T00:00:00.000-08:00")
+                                 (c/to-sql-time #inst "2020-12-31T23:59:59.000-08:00")]
+                                [:= :provider_id 1]]}))
+
+  (get-availabilities {:from "2020-08-12" :to "2020-08-31" :state "WA"})
+  (get-availabilities {:from "2020-08-12" :to "2020-08-31" :state "OR"})
 
   (d/create-need! {:name "Interpretation"
                    :description "Translation service for a non-English speaker"})
@@ -45,20 +82,15 @@
                           :provider-id 1})
 
   (d/get-appointment {:id 1})
-  (d/get-appointment {:id 2})
+  (d/get-appointment {:id 5})
 
-  (d/get-provider {:user-id 2})
+  (d/get-provider {:id 1})
 
   (d/create-availability! {:start (c/to-sql-time #inst "2020-08-11T11:30:00.000-08:00")
                            :end   (c/to-sql-time #inst "2020-08-11T17:00:00.000-08:00")
                            :provider-id 1})
 
-  (d/create-availability! {:start (c/to-sql-time #inst "2020-08-12T14:30:00.000-08:00")
-                           :end   (c/to-sql-time #inst "2020-08-12T18:00:00.000-08:00")
-                           :provider-id 1})
 
-  ;; TODO simplify this horrible API
-  ;; https://github.com/jkk/honeysql
   (d/get-availabilities {:join  (d/join-provider-availability)
                          :where (d/where-available
                                  {:cond
--- test/rtc/appointments_test.clj
+++ test/rtc/appointments_test.clj
@@ -1,8 +1,25 @@
 (ns rtc.appointments-test
   (:require
-   [clojure.test :refer [deftest is]]))
+   [clj-time.coerce :as c]
+   [clojure.test :refer [deftest is]]
+   [rtc.appointments.avail :as avail]))
 
 
-(deftest stuff
-  ;; FIXME
-  (is (true? true)))
+
+(deftest test-params->query
+  ;; No need to join when not querying by state
+  (is (nil? (:join (avail/params->query {}))))
+  ;; Join on provider_id
+  (is (= [[:providers :p] [:= :p.id :a.provider_id]]
+         (:join (avail/params->query {:state "CA"}))))
+  (is (= [:and [:= :state "NY"]]
+         (:where (avail/params->query {:state "NY"}))))
+  (is (= [:and
+          [:= :state "CA"]
+          [:between
+           :start_time
+           (c/to-sql-time "2020-01-01T10:00:00.000-08:00")
+           (c/to-sql-time "2020-01-07T10:00:00.000-08:00")]]
+         (:where (avail/params->query {:state "CA"
+                                       :from "2020-01-01T10:00:00.000-08:00"
+                                       :to "2020-01-07T10:00:00.000-08:00"})))))
