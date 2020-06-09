4650ff70bc7a4c2afb2c69c52f27c3cd81b16fca
Coby Tamayo <acobster@gmail.com>
2020-06-09 00:10:04 -0700

    make garden watcher more robust

--- src/rtc/style/build.clj
+++ src/rtc/style/build.clj
@@ -6,20 +6,29 @@
    [ns-tracker.core :refer [ns-tracker]]))
 
 
+(defn- compile-css-on-change [build]
+  (let [{:keys [source-paths styles compiler interval-ms]} build
+        tracker (ns-tracker source-paths)
+        ns-sym (symbol (namespace styles))]
+    (loop [namespaces (tracker)]
+      (try
+        (when (seq namespaces)
+          (require ns-sym :reload)
+          (let [styles-list (deref (resolve (:styles build)))
+                [conf rules] (if (map? (first styles-list))
+                               [(first styles-list) (next styles-list)]
+                               [{} styles-list])]
+            ;; TODO why doesn't this work the first time?
+            (apply garden/css (merge conf compiler) rules)))
+        (catch Exception e
+          (println "Error compiling CSS:" (.getMessage e) e)))
+      (Thread/sleep (or interval-ms 100))
+      (flush)
+      (recur (tracker)))))
+
 (defn watch!
   "Watch namespaces for changes, compiling CSS when a stylesheet or
    any of its dependencies is updated."
-  [{:keys [source-paths styles compiler interval-ms]}]
-  (let [tracker (ns-tracker source-paths)
-        ns-sym (symbol (namespace styles))]
-    ;; TODO use go-loop
-    (loop [namespaces (tracker)]
-      (when (seq namespaces)
-        (require ns-sym :reload)
-        (let [styles-list (deref (resolve styles))
-              [conf rules] (if (map? (first styles-list))
-                             [(first styles-list) (next styles-list)]
-                             [{} styles-list])]
-          (apply garden/css (merge conf compiler) rules)))
-      (Thread/sleep (or interval-ms 100))
-      (recur (tracker)))))
+  [build]
+  (.start (Thread. (fn []
+                     (compile-css-on-change build)))))
