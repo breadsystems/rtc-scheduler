f34228ee692f4ad757e35bfaf6e42f08c2981dcb
Coby Tamayo <acobster@gmail.com>
2020-06-11 22:11:38 -0700

    more robust button styles

--- src/rtc/intake/style.clj
+++ src/rtc/intake/style.clj
@@ -4,13 +4,22 @@
    [garden.def :refer [defstyles]]))
 
 
+(def nav
+  [[:ul.progress {:display :flex
+                  :margin "3em 0 3em"
+                  :padding 0
+                  :width "100%"
+                  :justify-content :space-around
+                  :list-style :none}]
+   [:.step-link {:text-decoration :none}]])
+
 (def questions
   [[:.question {:margin "2em 0"}]
    [:.field-label {:font-weight 700
                    :color core/dark-purple}]
    [:.intake-footer {:display :flex
                      :max-width "65em"
-                     :margin "5em auto"
+                     :margin "3em auto"
                      :justify-content :space-between}]])
 
 
@@ -18,4 +27,5 @@
   core/base
   core/typography
   core/forms
+  nav
   questions)
--- src/rtc/style/core.clj
+++ src/rtc/style/core.clj
@@ -31,50 +31,57 @@
    [:.help {:color grey
             :font-style :italic}]])
 
+
+(def disabled-button {:background-color muted-pink
+                      :color off-white
+                      :cursor :not-allowed})
+
+(def disabled-secondary-button
+  (merge disabled-button {:background-color muted-purple
+                          :color off-white}))
+
 (def forms
-  (let [disabled-button {:background-color muted-pink
-                         :color off-white
-                         :cursor :not-allowed}]
-    [[:.field {:margin "0.7em 0"}]
-     [:.radio-option {:margin "0 2em 0 0"}]
-     [:.required {:color pink}]
-     [:.has-errors {:color dark-purple
-                    :background-color light-pink
-                    :border-color muted-pink}]
-     [:.error-message {:color muted-pink
-                       :font-weight 700
-                       :margin "0.3em 0"}]
-     [:input [(& (attr :type=text))
-              (& (attr :type=email))
-              (& (attr :type=password))
-              {:min-width "25rem"
-               :padding "0.7em"
-               :border-radius "0.3em"
-               :border purple-border}]]
-     [:button {:padding "0.7em 1.3em"
-               :border :none
-               :cursor :pointer
-               :font-weight 700
-               :text-transform :uppercase
-               :color :white
-               :background pink}
-      [:&.next {:text-align :right}]
-      [:&:disabled disabled-button]]
-     [:select {:min-width "25rem"
-               :border purple-border
-               :border-radius "0.3em"
-               :background-color :lavender
-               :background-image (url "/img/caret-down.svg")
-               :background-position "right 10px bottom 12px"
-               :background-repeat :no-repeat
-               :padding "0.7em"
-               :-moz-appearance :none
-               :-webkit-appearance :none}
-      [:&:ms-expand {:display :none}]]
-     [:.fc-button {:opacity 1
-                   :background dark-purple
-                   :border :none
-                   :font-weight 700
-                   :text-transform :uppercase}
-      [:&:disabled (merge disabled-button {:background-color muted-purple
-                                           :color off-white})]]]))
+  [[:.field {:margin "0.7em 0"}]
+   [:.radio-option {:margin "0 2em 0 0"}]
+   [:.required {:color pink}]
+   [:.has-errors {:color dark-purple
+                  :background-color light-pink
+                  :border-color muted-pink}]
+   [:.error-message {:color muted-pink
+                     :font-weight 700
+                     :margin "0.3em 0"}]
+   [:input [(& (attr :type=text))
+            (& (attr :type=email))
+            (& (attr :type=password))
+            {:min-width "25rem"
+             :padding "0.7em"
+             :border-radius "0.3em"
+             :border purple-border}]]
+   [:button :.button {:padding "0.7em 1.3em"
+                      :border :none
+                      :cursor :pointer
+                      :font-weight 700
+                      :text-transform :uppercase
+                      :color :white
+                      :background pink}
+    [:&.secondary {:background purple}
+     [:&:disabled :&.disabled disabled-secondary-button]]
+    [:&.next {:text-align :right}]
+    [:&:disabled :&.disabled disabled-button]]
+   [:select {:min-width "25rem"
+             :border purple-border
+             :border-radius "0.3em"
+             :background-color :lavender
+             :background-image (url "/img/caret-down.svg")
+             :background-position "right 10px bottom 12px"
+             :background-repeat :no-repeat
+             :padding "0.7em"
+             :-moz-appearance :none
+             :-webkit-appearance :none}
+    [:&:ms-expand {:display :none}]]
+   [:.fc-button {:opacity 1
+                 :background dark-purple
+                 :border :none
+                 :font-weight 700
+                 :text-transform :uppercase}
+    [:&:disabled disabled-secondary-button]]])
