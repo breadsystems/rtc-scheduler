45f7e2f4f801592d56b027a79fba7137b830a216
Coby Tamayo <acobster@gmail.com>
2020-08-03 19:44:06 -0700

    less hacky version of window algorithm, still broken

--- src/rtc/appointments/windows.clj
+++ src/rtc/appointments/windows.clj
@@ -27,6 +27,99 @@
 
 
 
+;; (defn- apply-op [id->toggle [id op]]
+;;   (update id->toggle id + op))
+
+(defn- apply-ops [id->toggle [[_ id op] & ops]]
+  (if op
+    (update (apply-ops id->toggle ops) id #(+ (or % 0) op))
+    id->toggle))
+
+;; (defn- apply-op-at [date->id->toggle [t id op]]
+;;   (update date->id->toggle t #(update % id + op)))
+
+;; (defn- apply-ops [id->toggle [op & ops]]
+;;   (update id->toggle (fn [id])))
+
+
+(defn- toggles->pids [toggles]
+  (let [positives (filter (fn [[_ v]] (= 1 v)) toggles)]
+    (into [] (map key positives))))
+
+(defn- id->appt-count [id appts]
+  (count (filter #(= id (:id %)) appts)))
+
+(defn- curtail [earliest latest {:keys [start end] :as e}]
+  (assoc e
+         :start (max earliest start)
+         :end (max latest end)))
+
+(defn- avail->ops [from to w factor {:keys [start end id]}]
+  (if (or (> start to) ;; 
+          (> from end)
+          (> w (- from start))
+          (> w (- to end)))
+    []
+    (let [start (- (max start from) (mod (Math/abs (max start from)) w))
+          end   (+ (min end to) (mod (Math/abs (min end to)) w))]
+      [[start id factor] [end id (* factor -1)]])))
+
+
+(defn- fold-ops [t->ops [start id op]]
+  (update t->ops start conj [start id op]))
+
+(defn ->windows [avails appts from to w]
+  {:pre [(> to from)
+         (>= (- to from) w)
+         (pos-int? w)
+         ;; TODO validate avails
+         ;; TODO validate appts
+         ]}
+
+  (let [windows (zipmap (range from to w) (cycle [[]]))
+        curtail* (partial curtail from to)
+        all-ops (reduce concat []
+                        (concat (map (comp (partial avail->ops from to w 1) curtail*) avails)
+                                (map (comp (partial avail->ops from to w -1) curtail*) appts)))
+        _ (println all-ops)
+        _ (println all-ops)
+        id->toggle (atom {})
+        reducer (fn [windows [t ops]]
+                  (swap! id->toggle apply-ops ops)
+                  (let [pids (toggles->pids @id->toggle)]
+                    (if (seq pids)
+                      (conj windows {:start t
+                                     :end (+ t w)
+                                 ;; TODO sort-by appt-counts
+                                     :provider-ids pids}))))]
+    (reduce reducer [] windows)))
+
+(comment
+  (def pid->toggle {1 0, 2 0, 3 0})
+  (apply-ops pid->toggle [[3 1] [3 -1] [2 1]])
+  (toggles->pids (apply-ops {1 0, 2 0, 3 0} [[3 1] [3 -1] [2 1]]))
+
+
+  (def date->pid->toggle {0   {1 0, 2 0, 3 0}
+                          50  {1 0, 2 0, 3 0}
+                          100 {1 0, 2 0, 3 0}})
+
+  (apply-op-at date->pid->toggle [0 1 1])
+  (apply-ops* date->pid->toggle [[0 1 1] [0 1 -1] [0 2 1] [50 1 1] [100 2 1]])
+
+  (update date->pid->toggle 50 (fn [p->t]
+                                 (apply-ops p->t [[3 1]])))
+
+
+  (id->appt-count 1 [{:id 1} {:id 2} {:id 1}])
+
+  (reduce concat [] (concat [[[:op 1] [:op 2]] [[:op 3] [:op 4]]] [[[:op 5]] [[:op 6]]]))
+
+  ;;  
+  )
+
+
+
 (defn availabilities->windows
   "The core algorithm of the appointment calendar. Takes a list of availabilities,
    a list of appointments, a from/to date, and a window length (in minutes), and
@@ -99,5 +192,4 @@
                  :provider-id 1}
                 {:start #inst "2020-01-03T12:30"
                  :end   #inst "2020-01-03T15:00"
-                 :provider-id 2}]
-        ]))
+                 :provider-id 2}]]))
--- test/rtc/appointments_windows_test.clj
+++ test/rtc/appointments_windows_test.clj
@@ -26,6 +26,71 @@
 
 ;; The latest end time should be no later than `to`
 
+(deftest test-->windows
+  ;; This tests a simplified model of time using nice round numbers
+  ;; for readability. But the concept is exactly the same when applied
+  ;; to timestamps, which are just integers.
+  ;; In this simplified timeline:
+  ;; * hours are 100 minutes
+  ;; * days are 1000 minutes (10 hours)
+  ;; * the standard window length is 50
+  (let [;; Imaginary provider ids:
+        ;; * 1 - Shevek
+        ;; * 2 - Takver
+        ;; * 3 - Genly Ai
+        avails [;; DAY 0: only Takver
+                ;; 1-5 Takver
+                {:start 100 :end 500 :id 2}
+
+                ;; DAY 1: No availabilities.
+
+                ;; DAY 2: Shevek & Takver available w/ some overlap
+                ;; 2-4 Shevek
+                {:start 2200 :end 2500 :id 1}
+                ;; 3-6 Takver
+                {:start 2300 :end 2600 :id 2}
+
+                ;; DAY 3: Genly Ai available for two distinct timeframes
+                ;; 3-5 Genly
+                {:start 3300 :end 3500 :id 3}
+                ;; 7-9
+                {:start 3300 :end 3500 :id 3}]]
+
+;;     (testing "with some overlap at start"
+;;       (is (= [{:start 100 :end 150 :provider-ids [2]}
+;;               {:start 150 :end 200 :provider-ids [2]}
+;;               {:start 200 :end 250 :provider-ids [2]}
+;;               {:start 250 :end 300 :provider-ids [2]}]
+;;              (w/->windows avails [] 0 300 50))))
+
+    (testing "when `to` is before the first availability"
+      (is (= []
+             (w/->windows avails [] -1000 -500 50)))
+      (is (= []
+             (w/->windows avails [] -100 0 50)))
+      (is (= []
+             (w/->windows avails [] 0 50 50)))
+      (is (= []
+             (w/->windows avails [] 49 99 50))))
+
+    (testing "when `from` is after the last availability"
+      (is (= []
+             (w/->windows avails [] 3500 4000 50)))
+      (is (= []
+             (w/->windows avails [] 10000 20000 50))))
+
+    (testing "when duration is less than `w`"
+      (is (thrown? java.lang.AssertionError
+                   (w/->windows avails [] 0 10 50))))
+
+    (testing "when overlap is less than `w`"
+      (is (= []
+             (w/->windows avails [] 0 149 50)))
+      (is (= []
+             (w/->windows avails [] 451 1000 50))))
+    ;;
+    ))
+
 (deftest test-availabilities->windows
   (let [;; Imaginary provider ids:
         ;; * 1 - Shevek
