0c9610218e14225145c98f4ccdc1cf410b45d8c0
Coby Tamayo <acobster@gmail.com>
2020-08-13 22:51:11 -0700

    build intake and admin CSS at prod build time

--- env/prod/rtc/env.clj
+++ env/prod/rtc/env.clj
@@ -2,15 +2,21 @@
   (:require
    [garden.core :as garden]
    [mount.core :refer [defstate]]
+   [rtc.admin.style :as admin-style]
    [rtc.intake.style :as intake-style]))
 
 
 (defstate css
   ;; TODO MOAR STYLEZ
-  :start (apply garden/css
-                {:pretty-print? false
-                 :output-to "resources/public/css/intake.css"}
-                intake-style/screen))
+  :start (do
+           (apply garden/css
+                  {:pretty-print? false
+                   :output-to "resources/public/css/intake.css"}
+                  intake-style/screen)
+           (apply garden/css
+                  {:pretty-print? false
+                   :output-to "resources/public/css/admin.css"}
+                  admin-style/screen)))
 
 
 (defn middleware [handler]
