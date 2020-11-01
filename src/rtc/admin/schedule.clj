fb3ae46e0430683f932a99ed320522054db5fd73
Coby Tamayo <acobster@gmail.com>
2020-10-31 20:48:59 -0700

    parameterize schedule data by date range

--- src/rtc/admin/schedule.clj
+++ src/rtc/admin/schedule.clj
@@ -1,14 +1,30 @@
 (ns rtc.admin.schedule
   (:require
-    [rtc.appointments.appointments :as appt]
-    [rtc.appointments.avail :as avail]))
+   [rtc.appointments.appointments :as appt]
+   [rtc.appointments.avail :as avail]
+   [rtc.util :refer [index-by]])
+  (:import
+   [java.util Date]))
 
+(defonce one-week (* 7 24 60 60 1000))
 
-(defn schedule [_]
-  ;; TODO parameterize from/to
-  {:appointments   (appt/get-appointments {})
-   :availabilities (avail/get-availabilities {})
-   :users          []})
+(defn- one-week-ago []
+  (- (inst-ms (Date.)) one-week))
+
+(defn- one-week-from-now []
+  (+ (inst-ms (Date.)) one-week))
+
+(defn schedule [{:keys [query-params] :as req}]
+  {:appointments
+   (index-by :id (appt/get-appointments
+                  {:from (one-week-ago)
+                   :to (* 8 (one-week-from-now))}))
+   :availabilities
+   (index-by :id (avail/get-availabilities
+                  {:from (one-week-ago)
+                   :to (* 8 (one-week-from-now))}))
+   :users          []
+   :params query-params})
 
 
 (comment
