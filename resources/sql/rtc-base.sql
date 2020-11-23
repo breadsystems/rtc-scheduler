fbebb5a8489dcc5bc6e30a1f9c9e9ef8abbff25d
Coby Tamayo <acobster@gmail.com>
2020-11-22 23:00:05 -0800

    remove a bunch of dead SQL queries

--- resources/sql/rtc-base.sql
+++ resources/sql/rtc-base.sql
@@ -12,9 +12,7 @@ SELECT * FROM users WHERE id = :id
 
 -- :name get-user-by-email :? :1
 -- :doc retrieves a user record given an email
-SELECT u.*, p.state, p.id != NULL AS is_provider FROM users u
-LEFT JOIN providers p ON u.id = p.id
-WHERE email = :email
+SELECT u.* FROM users u WHERE email = :email
 
 -- :name delete-user! :! :n
 -- :doc deletes a user record given the id
@@ -39,25 +37,6 @@ VALUES (:email, :code, now(), :invited_by, false)
 -- :doc redeem an existing email/code invitation combo
 UPDATE invitations SET redeemed = true
 WHERE email = :email AND code = :code AND now() < (date_invited + interval '72 hours')
-
-
--- :name create-provider! :! :n
--- :doc create a new provider record
-INSERT INTO providers (state, id, date_created, date_modified)
-VALUES (:state, :id, now(), now())
-
--- :name update-provider! :! :n
--- :doc update an existing provider record
-UPDATE providers SET state = :state, date_modified = now() WHERE id = :id
-
--- :name get-provider :? :1
--- :doc retrieve a provider by their id
-SELECT providers.id, state, email, first_name, last_name, pronouns, phone
-FROM providers JOIN users ON (providers.id = users.id) WHERE providers.id = :id
-
--- :name delete-provider! :! :n
--- :doc delete a provider record given the id
-DELETE FROM providers WHERE id = :id
 
 -- :name create-appointment-need! :! :n
 -- :doc create an appointment_need record
@@ -87,61 +66,6 @@ WHERE need_id = :need-id AND appointment_id = :appointment-id
 -- :name get-appointment :? :1
 -- :doc get an appointment by its id
 SELECT id, start_time, end_time, careseeker_id, provider_id FROM appointments WHERE id = :id
-
--- :name get-appointment-need :n
--- :doc get an appointment_need by its need/appointment ids
-SELECT need_id, appointment_id, info, contact_id,
-appt.contact_id, c.full_name, c.company_name, c.title, c.phone, c.email
-FROM appointment_needs AS appt
-LEFT JOIN contacts c ON c.id = appt.contact_id
-WHERE appointment_id = :appointment-id
-
--- :name delete-appointment! :! :n
--- :doc delete an existing appointment record by its id
-DELETE FROM appointments WHERE id = :id
-
--- :name delete-appointment-need! :! :n
--- :doc delete an existing appointment_need record by its id
-DELETE FROM appointment_needs WHERE need_id = :need-id AND appointment_id = :appointment-id
-
-
--- :name create-availability! :! :n
--- :doc create a provider availability record
-INSERT INTO availabilities (start_time, end_time, provider_id) VALUES (:start, :end, :provider-id)
-
--- :name update-availability! :! :n
--- :doc update an existing availability record
-UPDATE availabilities
-SET start_time = :start, end_time = :end, provider_id = :provider-id WHERE id = :id
-
--- :snip join-provider-availability
-LEFT JOIN providers ON availabilities.provider_id = providers.id
-
--- :snip available-between
-:sql:conj (start_time BETWEEN :v:start AND :v:end)
-
--- :snip available-in-state
-:sql:conj (providers.state = :v:state)
-
--- :snip where-available
-WHERE :snip*:cond
-
--- :name get-availabilities :n
--- :doc get availabilities
-SELECT * FROM availabilities
---~ (when (:join params) ":snip:join")
---~ (when (:where params) ":snip:where")
-
--- Example:
--- (get-availabilities {:where
---                      (where-available
---                       {:cond
---                        [(available-between {:start 15234... :end 15234...})
---                         (available-in-state {:state "WA"})]})})
-
--- :name delete-availability :! :n
--- :doc delete an existing availability record by its id
-DELETE FROM availabilities WHERE id = :id
 
 
 -- :name create-need! :! :n
