202415d2f1922233c483e83ee2f3be4f071bf4a1
Coby Tamayo <acobster@gmail.com>
2020-11-23 00:17:25 -0800

    display real data on the schedule

--- src/rtc/admin/schedule.clj
+++ src/rtc/admin/schedule.clj
@@ -1,5 +1,6 @@
 (ns rtc.admin.schedule
   (:require
+   [clojure.set :refer [rename-keys]]
    [rtc.appointments.appointments :as appt]
    [rtc.appointments.availabilities :as avail]
    [rtc.db :as d]
@@ -7,6 +8,7 @@
   (:import
    [java.util Date]))
 
+(rename-keys {:x :y} {:x :x/x})
 (defonce one-week (* 7 24 60 60 1000))
 
 (defn- one-week-ago []
@@ -15,18 +17,27 @@
 (defn- one-week-from-now []
   (+ (inst-ms (Date.)) one-week))
 
-(defn schedule [{:keys [query-params] :as req}]
-  {:appointments
-   (index-by :id (appt/get-appointments
-                  {:from (one-week-ago)
-                   :to (* 8 (one-week-from-now))}))
-   :availabilities
-   (index-by :id (avail/get-availabilities
-                  {:from (one-week-ago)
-                   :to (* 8 (one-week-from-now))}))
-   :users
-   (index-by :id (d/get-all-providers))
-   :params query-params})
+(defn schedule [{:keys [query-params]}]
+  (let [appts (appt/get-appointments
+               {:from (one-week-ago)
+                :to (* 8 (one-week-from-now))})
+        avails (avail/get-availabilities
+                {:from (one-week-ago)
+                 :to (* 8 (one-week-from-now))})
+        key-mapping {:provider_id :user/id
+                     :start_time :start
+                     :end_time :end}]
+    {:appointments
+     (as-> appts $
+       (map #(rename-keys % key-mapping) $)
+       (index-by :id $))
+     :availabilities
+     (as-> avails $
+       (map #(rename-keys % key-mapping) $)
+       (index-by :id $))
+     :users
+     (index-by :id (d/get-all-providers))
+     :params query-params}))
 
 
 (comment
