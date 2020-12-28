75b9b042706b4613e05b5a565129056a495c7345
Coby Tamayo <acobster@gmail.com>
2020-12-28 14:26:28 -0800

    fix password tests

--- test/rtc/users_passwords_test.clj
+++ test/rtc/users_passwords_test.clj
@@ -5,16 +5,16 @@
 
 
 (deftest test-validate-passwords
-  (is (= {:password [{:message "Please enter a password"}]
-          :password-confirmation []}
-         (pass/validate-passwords {:password ""
-                                         :password-confirmation "anything"})))
-  (is (= {:password []
-          :password-confirmation [{:message "Passwords do not match"}]}
-         (pass/validate-passwords {:password "something"
-                                         :password-confirmation "else"})))
-  (is (= {:password [{:message "Please choose a longer password"}]
-          :password-confirmation []}
-         (pass/validate-passwords {:password "1234567"})))
-  (is (true? (pass/validate-passwords {:password "longpassword"
-                                             :password-confirmation "longpassword"}))))
+  (is (= {:pass [{:message "Please enter a password"}]
+          :pass-confirmation []}
+         (pass/validate-passwords {:pass ""
+                                   :pass-confirmation "anything"})))
+  (is (= {:pass []
+          :pass-confirmation [{:message "Passwords do not match"}]}
+         (pass/validate-passwords {:pass "something"
+                                   :pass-confirmation "else"})))
+  (is (= {:pass [{:message "Please choose a longer password"}]
+          :pass-confirmation []}
+         (pass/validate-passwords {:pass "1234567"})))
+  (is (true? (pass/validate-passwords {:pass "longpassword"
+                                       :pass-confirmation "longpassword"}))))
