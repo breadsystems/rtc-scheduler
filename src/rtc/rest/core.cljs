92906bf26bed2eb27f33c8fef01ac47f1dd7b6a8
Coby Tamayo <acobster@gmail.com>
2020-12-28 16:50:56 -0800

    one-stop-shop for anti-forgery POST reqs

--- src/rtc/rest/core.cljs
+++ src/rtc/rest/core.cljs
@@ -8,6 +8,9 @@
    [cljs.core.async.macros :refer [go]]))
 
 
+(defn- csrf-token []
+  (.-content (js/document.querySelector "meta[name=csrf-token]")))
+
 (defn request! [http-method endpoint req then on-err]
   (go (let [response (<! (http-method endpoint req))
             reader (transit/reader :json)]
@@ -26,8 +29,10 @@
 (defn get! [& args]
   (apply request! http/get args))
 
-(defn post! [& args]
-  (apply request! http/post args))
+(defn post! [endpoint req & args]
+  (let [secure-req
+        (update-in req [:headers "x-csrf-token"] #(or % (csrf-token)))]
+    (apply request! http/post endpoint secure-req args)))
 
 (defn patch! [& args]
   (apply request! http/patch args))
