d877b5f67c0f45f97ccefd56a8668a99f3c329cb
Coby Tamayo <acobster@gmail.com>
2021-12-15 10:03:29 -0800

    add default values (false) for reminded_* columns

--- resources/migrations/20211215015932-appointment-reminders.up.sql
+++ resources/migrations/20211215015932-appointment-reminders.up.sql
@@ -1,4 +1,4 @@
 -- Track appointment reminders in the appointments table.
 ALTER TABLE appointments
-ADD COLUMN reminded_careseeker BOOLEAN,
-ADD COLUMN reminded_provider BOOLEAN;
+ADD COLUMN reminded_careseeker BOOLEAN DEFAULT false,
+ADD COLUMN reminded_provider BOOLEAN DEFAULT false;
