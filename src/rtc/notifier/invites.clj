8b63a2db254d2dc9d4809c3b35377dfe09072b69
Coby Tamayo <coby@tamayo.email>
2022-06-24 08:34:37 -0700

    formatting

--- src/rtc/notifier/invites.clj
+++ src/rtc/notifier/invites.clj
@@ -3,14 +3,13 @@
     [rtc.notifier.sendgrid :as sendgrid]))
 
 (defn- invite->email [{:keys [email url]}]
-  (let [message
-        (format (str "Welcome to the Radical Telehealth Collective!\n\n"
-                     "Go here to redeem your invite"
-                     " and set up your account: %s\n\n")
-                url)]
-    {:subject "Your Radical Telehealth Collective invite"
-     :to email
-     :message message}))
+  {:subject "Your Radical Telehealth Collective invite"
+   :to email
+   :message
+   (format
+     (str "Welcome to the Radical Telehealth Collective!\n\n"
+          "Go here to redeem your invite and set up your account: %s\n\n")
+     url)})
 
 (defn invited! [invite]
   (sendgrid/send-email! (invite->email invite)))
